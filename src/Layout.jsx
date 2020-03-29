d489f46355ef1ed383dbd39edd78a0079d061370
Jimmy DC <jimmydalecleveland@gmail.com>
2020-03-29 15:14:59 -0600

    abstract reducer, useInterval, constants

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,102 +1,14 @@
-import React, { useEffect, useRef, useReducer } from 'react'
-import * as Styled from './Layout.styles'
+import React, { useReducer } from 'react'
 
+import reducer from './reducer'
+import useInterval from './useInterval'
 import Tomato from './Tomato'
-
-const TWENTY_FIVE_MINUTES_IN_SECONDS = 3
-// const TWENTY_FIVE_MINUTES_IN_SECONDS = 1500
-const FIVE_MINUTES_IN_SECONDS = 300
-
-/**
- * Reducer function to be used with useReducer for all pomodoro state
- * @param {Object} state - State object
- * @param {Object} action
- * @property {string} type - Action to run switch against
- * @property {any} [payload] - Any additional information needed to update state
- */
-
-function reducer(state, action) {
-  const { time, pomo, isTimerActive, areAlertsOn } = state
-  if (action.type !== 'DECREMENT_TIMER') {
-    console.log(
-      '%cDispatch Fired',
-      'color: tomato; font-weight: bold',
-      `
-      type: ${action.type}
-      payload: ${action.payload}
-    `,
-      state
-    )
-  }
-
-  switch (action.type) {
-    case 'TOGGLE_ALERTS': {
-      return { ...state, areAlertsOn: !areAlertsOn }
-    }
-    case 'DECREMENT_TIMER': {
-      return { ...state, time: time - 1 }
-    }
-    case 'START_NEW_POMO': {
-      const nextPomo = pomo === 4 ? 1 : pomo + 1
-
-      return { ...state, isPomoActive: true, pomo: nextPomo }
-    }
-    case 'TOGGLE_TIMER_ACTIVE': {
-      return { ...state, isTimerActive: !isTimerActive }
-    }
-    case 'UPDATE_TIME': {
-      return { ...state, time: action.payload }
-    }
-    case 'START_SHORT_BREAK': {
-      return { ...state, time: FIVE_MINUTES_IN_SECONDS, isPomoActive: false }
-    }
-    case 'START_LONG_BREAK': {
-      return {
-        ...state,
-        time: TWENTY_FIVE_MINUTES_IN_SECONDS,
-        isPomoActive: false,
-      }
-    }
-    case 'END_BREAK': {
-      const wasLongBreak = pomo === 4
-
-      return {
-        ...state,
-        time: TWENTY_FIVE_MINUTES_IN_SECONDS,
-        isTimerActive: false,
-        pomo: wasLongBreak ? 0 : pomo,
-      }
-    }
-    default: {
-      return state
-    }
-  }
-}
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef()
-
-  useEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
-
-  useEffect(() => {
-    function tick(id) {
-      savedCallback.current(id)
-    }
-
-    if (delay !== null) {
-      const id = setInterval(() => tick(id), delay)
-      return () => clearInterval(id)
-    }
-
-    return undefined
-  }, [delay])
-}
+import * as Styled from './Layout.styles'
+import { TWENTY_FIVE_MINUTES_IN_SECONDS } from './utils'
 
 const initialState = {
   time: TWENTY_FIVE_MINUTES_IN_SECONDS,
-  pomo: 3,
+  pomo: 0,
   isPomoActive: false,
   isTimerActive: false,
   areAlertsOn: true,
---
+++ src/reducer.js
@@ -0,0 +1,71 @@
+import {
+  FIVE_MINUTES_IN_SECONDS,
+  TWENTY_FIVE_MINUTES_IN_SECONDS,
+} from './utils'
+
+/**
+ * Reducer function to be used with useReducer for all pomodoro state
+ * @param {Object} state - State object
+ * @param {Object} action
+ * @property {string} type - Action to run switch against
+ * @property {any} [payload] - Any additional information needed to update state
+ */
+function reducer(state, action) {
+  const { time, pomo, isTimerActive, areAlertsOn } = state
+  if (action.type !== 'DECREMENT_TIMER') {
+    console.log(
+      '%cDispatch Fired',
+      'color: tomato; font-weight: bold',
+      `
+      type: ${action.type}
+      payload: ${action.payload}
+    `,
+      state
+    )
+  }
+
+  switch (action.type) {
+    case 'TOGGLE_ALERTS': {
+      return { ...state, areAlertsOn: !areAlertsOn }
+    }
+    case 'DECREMENT_TIMER': {
+      return { ...state, time: time - 1 }
+    }
+    case 'START_NEW_POMO': {
+      const nextPomo = pomo === 4 ? 1 : pomo + 1
+
+      return { ...state, isPomoActive: true, pomo: nextPomo }
+    }
+    case 'TOGGLE_TIMER_ACTIVE': {
+      return { ...state, isTimerActive: !isTimerActive }
+    }
+    case 'UPDATE_TIME': {
+      return { ...state, time: action.payload }
+    }
+    case 'START_SHORT_BREAK': {
+      return { ...state, time: FIVE_MINUTES_IN_SECONDS, isPomoActive: false }
+    }
+    case 'START_LONG_BREAK': {
+      return {
+        ...state,
+        time: TWENTY_FIVE_MINUTES_IN_SECONDS,
+        isPomoActive: false,
+      }
+    }
+    case 'END_BREAK': {
+      const wasLongBreak = pomo === 4
+
+      return {
+        ...state,
+        time: TWENTY_FIVE_MINUTES_IN_SECONDS,
+        isTimerActive: false,
+        pomo: wasLongBreak ? 0 : pomo,
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+export default reducer
